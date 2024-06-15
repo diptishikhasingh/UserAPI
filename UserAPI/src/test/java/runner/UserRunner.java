119e660974555edb5fc7ee2a04adc492db067f42
diptishikhasingh <shaileshverma@shaileshs-mbp>
2024-06-15 14:17:07 -0500

    First Commit: added Framework

---
+++ UserAPI/src/test/java/runner/UserRunner.java
@@ -0,0 +1,19 @@
+package runner;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"src/test/resources/features"},
+glue = {"stepDefinitions"},
+monochrome = true,
+plugin = {"pretty",
+"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+"html:target/cucmberreport",
+"html:target/JUNITHtmlReports/report.html",
+"junit:target/JUNITReports/reports.xml"})
+
+public class UserRunner {
+
+}
